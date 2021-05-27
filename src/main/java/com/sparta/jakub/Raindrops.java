190c2bda386ac0680c91c38e67a62fb358e013e4
Jakub Matyjewicz <niewiem826@gmail.com>
2021-05-27 20:22:24 +0100

    added implementation for plingPlangPlong method in Raindrops app

--- src/main/java/com/sparta/jakub/Raindrops.java
+++ src/main/java/com/sparta/jakub/Raindrops.java
@@ -1,4 +1,23 @@
 package com.sparta.jakub;
 
 public class Raindrops {
+    public String plingPlangPlong(int number) {
+
+        StringBuilder outputBuilder = new StringBuilder();
+
+        if (number % 3 == 0) {
+            outputBuilder.append("Pling");
+        }
+        if (number % 5 == 0) {
+            outputBuilder.append("Plang");
+        }
+        if (number % 7 == 0) {
+            outputBuilder.append("Plong");
+        }
+        if (number == 0 || outputBuilder.length() == 0){
+            return Integer.toString(number);
+        }
+
+        return outputBuilder.toString();
+    }
 }

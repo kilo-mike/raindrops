afa96efeca4fcffdad0bfb7de2d54d28bfede046
Jakub Matyjewicz <niewiem826@gmail.com>
2021-05-27 18:52:01 +0100

    added tests for output return Pling

--- src/test/java/com/sparta/jakub/RaindropsTests.java
+++ src/test/java/com/sparta/jakub/RaindropsTests.java
@@ -1,4 +1,25 @@
 package com.sparta.jakub;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 public class RaindropsTests {
+
+    Raindrops raindrops = new Raindrops();
+
+    @Nested
+    @DisplayName("Testing for single return")
+    class testingForSingleReturn {
+
+        @ParameterizedTest
+        @ValueSource(ints = {-27, -9, -6, -3, 3, 6, 9, 27})
+        @DisplayName("Testing  output return only Pling")
+        void testingOutputReturnOnlyPling(int number) {
+            Assertions.assertEquals("Pling", raindrops.plingPlangPlong(number));
+
+        }
+    }
 }
